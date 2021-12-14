6c3e259da4a1fc634c4fbdc622e2dcacff144525
MaximSafonov1 <Safonovmaximus@mail.ru>
2021-12-14 10:51:27 +0300

    Initial commit

---
+++ schema.py
@@ -0,0 +1,17 @@
+
+AD_CREATE = {
+	"type": "object",
+	"properties": {
+		"title": {
+			"type": "string"
+		},
+		"text": {
+			"type": "string"
+		},
+		"id_owner": {
+			"type": "number",
+			"minimum": 1
+		},
+	},
+	"required": ["title", "text", "id_owner"]
+}
